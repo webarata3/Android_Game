4621c5a155f271b9225fc7f1bb9bf1a600043c6a
webarata3 <webarata3@gmail.com>
2017-11-24 19:42:50 +0900

    Game05

---
+++ Game05/app/src/main/java/com/example/game05/GameView.java
@@ -0,0 +1,111 @@
+package com.example.game05;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class GameView extends View {
+    private static final Paint PAINT = new Paint();
+    private Droid droid;
+
+    private Bitmap leftArrow;
+    private Bitmap rightArrow;
+
+    private Rect leftRect;
+    private Rect rightRect;
+
+    private boolean pushLeftArrow;
+    private boolean pushRightArrow;
+
+    public GameView(Context context) {
+        super(context);
+
+        final int FPS = 60;
+        final Handler handler = new Handler();
+        final Runnable requestRedraw = new Runnable() {
+            @Override
+            public void run() {
+                invalidate();
+            }
+        };
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(requestRedraw);
+            }
+        };
+        Timer timer = new Timer(false);
+        timer.schedule(timerTask, 0, 1000 / FPS);
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        if (droid == null) {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.droid);
+            bitmap = Bitmap.createScaledBitmap(bitmap, 100, 100, false);
+            droid = new Droid(bitmap, canvas.getWidth(), canvas.getHeight());
+        }
+
+        if (leftArrow == null) {
+            leftArrow = BitmapFactory.decodeResource(getResources(), R.drawable.left_arrow);
+            leftArrow = Bitmap.createScaledBitmap(leftArrow, 64, 64, false);
+            int left = 0;
+            int top = canvas.getHeight() - leftArrow.getHeight();
+            leftRect = new Rect(left, top, left + leftArrow.getWidth(), top + leftArrow.getHeight());
+        }
+        if (rightArrow == null) {
+            rightArrow = BitmapFactory.decodeResource(getResources(), R.drawable.right_arrow);
+            rightArrow = Bitmap.createScaledBitmap(rightArrow, 64, 64, false);
+            int left = canvas.getWidth() - rightArrow.getWidth();
+            int top = canvas.getHeight() - rightArrow.getHeight();
+            rightRect = new Rect(left, top, left + rightArrow.getWidth(), top + rightArrow.getHeight());
+        }
+
+        canvas.drawBitmap(leftArrow, leftRect.left, leftRect.top, PAINT);
+        canvas.drawBitmap(rightArrow, rightRect.left, rightRect.top, PAINT);
+
+        if (pushLeftArrow) {
+            droid.moveLeft();
+        }
+        if (pushRightArrow) {
+            droid.moveRight();
+        }
+
+        droid.draw(canvas);
+    }
+
+    @Override
+    public boolean performClick() {
+        super.performClick();
+        return true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        performClick();
+
+        pushLeftArrow = false;
+        pushRightArrow = false;
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                if (leftRect.contains((int) event.getX(), (int) event.getY())) {
+                    pushLeftArrow = true;
+                } else if (rightRect.contains((int) event.getX(), (int) event.getY())) {
+                    pushRightArrow = true;
+                }
+                return true;
+            case MotionEvent.ACTION_UP:
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+}
