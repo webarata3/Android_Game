6f10461c07e5f370de362442ba895d4c28cf30ed
webarata3 <webarata3@gmail.com>
2017-11-25 16:13:27 +0900

    Game09

--- Game09/app/src/main/java/com/example/game09/GameView.java
+++ Game09/app/src/main/java/com/example/game09/GameView.java
@@ -128,6 +128,6 @@ public class GameView extends View {
                 pushRightArrow = false;
                 break;
         }
-        return super.onTouchEvent(event);
+        return true;
     }
 }
