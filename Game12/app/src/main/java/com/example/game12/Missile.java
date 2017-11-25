2bd99f0cd9f6c62f3897718aac73449ff7de1479
webarata3 <webarata3@gmail.com>
2017-11-25 13:20:07 +0900

    Game12

---
+++ Game12/app/src/main/java/com/example/game12/Missile.java
@@ -0,0 +1,53 @@
+package com.example.game12;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+
+import java.util.Random;
+
+public class Missile {
+    private static final Paint PAINT = new Paint();
+
+    private int missileSpeed;
+    private int canvasWidth;
+    private int canvasHeight;
+
+    private final int MISSILE_WIDTH;
+    private final int MISSILE_HEIGHT;
+
+    private Rect rect;
+
+    public Rect getRect() {
+        return rect;
+    }
+
+    public Missile(int missileSpeed, int missileSize, int canvasWidth, int canvasHeight) {
+        MISSILE_WIDTH = missileSize;
+        MISSILE_HEIGHT = missileSize * 4;
+        rect = new Rect(0, 0, MISSILE_WIDTH, MISSILE_HEIGHT);
+        this.missileSpeed = missileSpeed;
+        this.canvasWidth = canvasWidth;
+        this.canvasHeight = canvasHeight;
+        initMissile();
+    }
+
+    public void initMissile() {
+        Random random = new Random();
+        int x = random.nextInt(canvasWidth - MISSILE_WIDTH);
+        rect.offsetTo(x, -MISSILE_HEIGHT);
+    }
+
+    public void move() {
+        rect.offset(0, missileSpeed);
+        if (rect.top > canvasHeight) {
+            initMissile();
+        }
+    }
+
+    public void draw(Canvas canvas) {
+        PAINT.setColor(Color.RED);
+        canvas.drawRect(rect, PAINT);
+    }
+}
