041d0be4508f612c424cce2c88f455d5c63edb4b
webarata3 <webarata3@gmail.com>
2018-01-19 19:47:19 +0900

    ハイスコアを保管

---
+++ Game17/app/src/main/java/com/example/game/Arrow.java
@@ -0,0 +1,35 @@
+package com.example.game;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+
+public class Arrow {
+    private final Paint PAINT = new Paint();
+
+    public enum Direction {
+        LEFT, RIGHT;
+    }
+
+    private Rect rect;
+    private Bitmap bitmap;
+
+    public Arrow(Bitmap bitmap, int canvasWidth, int canvasHeight, Direction direction) {
+        int left = 0;
+        if (direction == Direction.RIGHT) {
+            left = canvasWidth - bitmap.getWidth();
+        }
+        int top = canvasHeight - bitmap.getHeight();
+        rect = new Rect(left, top, left + bitmap.getWidth(), top + bitmap.getHeight());
+        this.bitmap = bitmap;
+    }
+
+    public void draw(Canvas canvas) {
+        canvas.drawBitmap(bitmap, rect.left, rect.top, PAINT);
+    }
+
+    public boolean contains(int x, int y) {
+        return rect.contains(x, y);
+    }
+}
